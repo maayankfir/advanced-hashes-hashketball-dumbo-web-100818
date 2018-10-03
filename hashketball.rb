d5a62cd06878d4e2fef1424250e4ae9db2d31dfc
Maayan Kfir <mayankfir@gmail.com>
2018-10-03 15:51:02 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,7 +118,6 @@ end
 
 def num_points_scored(player_name)
 game_hash.each do |location, team_info|
-  binding.pry
 if team_info[:players].include?(player_name)
   return team_info[:players][player_name][:points]
 end
